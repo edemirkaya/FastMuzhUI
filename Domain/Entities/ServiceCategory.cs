f7dfa84320af8b40c07c31c0909ef0cbd937d320
Ertugrul DEMIRKAYA <ertugrulldemirkaya@gmail.com>
2024-09-08 18:19:54 +0300

    ServiceCategory created.

---
+++ Domain/Entities/ServiceCategory.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+using System.Reflection.Metadata;
+
+namespace Domain.Entities;
+
+public class ServiceCategory:Entity<Guid>
+{
+    public Guid SubServiceCategory { get; set; }
+    public string Name { get; set; }
+
+    public ServiceCategory()
+    {}
+
+    public ServiceCategory(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public ServiceCategory(Guid id, Guid subServiceCategory,string name)
+    {
+        Id= id;
+        SubServiceCategory = subServiceCategory;
+        Name = name;
+    }
+}
