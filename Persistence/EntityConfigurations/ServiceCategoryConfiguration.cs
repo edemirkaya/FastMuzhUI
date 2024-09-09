06332d45aceb1f16179e5f3bfdd2cccd8f1d78e7
Ertugrul DEMIRKAYA <ertugrulldemirkaya@gmail.com>
2024-09-10 00:18:40 +0300

    DbContext - Db Configuration and repository implementations

---
+++ Persistence/EntityConfigurations/ServiceCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations;
+
+public class ServiceCategoryConfiguration : IEntityTypeConfiguration<ServiceCategory>
+{
+    public void Configure(EntityTypeBuilder<ServiceCategory> builder)
+    {
+        builder.ToTable("ServiceCategory").HasKey(key => key.Id);
+
+        builder.Property(sc => sc.Id).HasColumnName("Id").IsRequired();
+        builder.Property(sc => sc.SubServiceCategory).HasColumnName("SubServiceCategory");
+        builder.Property(sc => sc.Name).HasColumnName("Name");
+        builder.Property(sc => sc.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(sc => sc.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(sc => sc.DeletedDate).HasColumnName("DeletedDate");
+        builder.Property(sc => sc.IsActive).HasColumnName("IsActive").IsRequired();
+
+        builder.HasQueryFilter(filter => !filter.DeletedDate.HasValue);
+    }
+}
---
+++ Persistence/Repositories/ServiceCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class ServiceCategoryRepository : EfRepositoryBase<ServiceCategory, Guid, BaseDbContext>, IServiceCategoryRepository
+{
+    public ServiceCategoryRepository(BaseDbContext dbcontext) : base(dbcontext)
+    {
+    }
+}
