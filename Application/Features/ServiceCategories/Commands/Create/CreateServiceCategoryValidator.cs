e165ebfe424d9ed4054b0d2824f9191a7c97b92c
Ertugrul DEMIRKAYA <ertugrulldemirkaya@gmail.com>
2024-09-26 00:49:02 +0300

    Fluent Validation - ServiceCategory Validation

---
+++ Application/Features/ServiceCategories/Commands/Create/CreateServiceCategoryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ServiceCategories.Commands.Create;
+
+public class CreateServiceCategoryValidator:AbstractValidator<CreateServiceCategoryCommand>
+{
+    public CreateServiceCategoryValidator()
+    {
+        RuleFor(r=> r.Name).NotEmpty().MinimumLength(2);
+    }
+}
